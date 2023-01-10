e311038f65d2525499341a548bd0cd68df0e3467
dstruiksma <detmer56@gmail.com>
2023-01-10 16:28:19 +0100

    telemetry with mockdata

---
+++ telemetry/receiver.py
@@ -0,0 +1,37 @@
+from socketserver import StreamRequestHandler, TCPServer
+import json
+
+
+IP = '127.0.0.1'
+PORT = 5000
+
+def print_data(data: bytes) -> None:
+    data = json.loads(data)
+    print(data)
+
+# This class handles receiving JSON packets from the transmitter and calls print_data
+class DumpHandler(StreamRequestHandler):
+    def handle(self) -> None:
+        """receive json packets from client"""
+        print('connection from {}:{}'.format(*self.client_address))
+        try:
+            while True:
+                data = self.rfile.readline()
+                if not data:
+                    break
+                print_data(data)
+        finally:
+            print('disconnected from {}:{}'.format(*self.client_address))
+
+def main() -> None:
+    server_address = (IP, PORT)
+    print('starting up on {}:{}'.format(*server_address))
+    with TCPServer(server_address, DumpHandler) as server:
+        print('waiting for a connection')
+        server.serve_forever()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
